080a19157c8f8e158326c0c77564c3ab8f53333c
JakobMenih <jakob.menih@gmail.com>
2022-02-09 19:56:00 +0100

    Editing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,13 @@ module ApplicationHelper
   def display_session_links
       capture do
         if user_signed_in?
-          concat link_to "Odjava", destroy_user_session_path, method: :delete
-        else
-          concat link_to "Prijava", new_user_session_path
+          concat link_to "Edit profile", edit_user_registration_path
           concat " "
-          concat link_to "Registracija", new_user_registration_path
+          concat link_to "Sign out", destroy_user_session_path, method: :delete
+        else
+          concat link_to "Log in", new_user_session_path
+          concat " "
+          concat link_to "Registration", new_user_registration_path
           concat " "
         end
       end
