c0ce49a684a78b55eb20daf2ee1cc105200107d4
JakobMenih <jakob.menih@gmail.com>
2022-02-22 13:29:19 +0100

    Comentator ki ne dela

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,7 @@ module ApplicationHelper
           concat link_to "Edit profile", edit_user_registration_path
           concat " "
           concat link_to "Sign out", destroy_user_session_path, method: :delete
+          concat " "
         else
           concat link_to "Log in", new_user_session_path
           concat " "
